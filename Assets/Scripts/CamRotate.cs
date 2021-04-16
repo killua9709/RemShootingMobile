4753e2bad5c2bd6e1abdc926b5eda3000ef2a0c6
leekwangsub <79181792+leekwangsub@users.noreply.github.com>
2021-04-16 17:22:47 +0900

    first commit
    첫 커밋

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    public float m_speed = 100f; //카메라를 돌릴때의 속도
+    public Transform playerBody; //카메라가 돌때 돌릴 플레이어의 몸의 변화를 위한 변수
+    float xRotation = 0f; //
+    //float mX;
+    //float mY;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;//마우스를 잠근다
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * m_speed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * m_speed * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        Vector3 dir = new Vector3(mouseX, 0f, mouseY);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float playerMovespeed = 5;
+    public float gravity = -20; // 중력가속도 원래는 9.8인데 크기 20으로 일단 설정
+    float yVelocity; //얘가 중력
+
+    public float jumpPower = 5; // 점프 파워를 5로 설정, unity에서 수정가능
+    public int maxJumpCount = 2; //2단 점프이므로 점프 제한 횟수를 2회로설정
+    int jumpCount = 0; //현재 점프 횟수
+
+    readonly int c_animHashKeyState = Animator.StringToHash("State");
+    readonly int c_animHashKeyDie = Animator.StringToHash("Die");
+    [SerializeField] State m_state = State.Idle;
+    Animator m_animator;
+
+    //[Header(("State"))
+
+    enum State : int
+    {
+        Idle = 0,
+        Run = 1,
+        Back=2
+    }
+
+    CharacterController cc;//character controller이용
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        cc = gameObject.GetComponent<CharacterController>(); //나의 컴포넌트 중에 character controller를 가져온다
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); //사용자 입력처리
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v); //움직이는 것이 y축이 아니라 x,z축만을 이동한다.
+        dir.Normalize();//정규화
+
+        dir = Camera.main.transform.TransformDirection(dir); //내가 바라보는 방향으로 (카메라의 방향으로)이동하고 싶다.
+
+        //-90에서 90까지의 y축좌표기준 회전일때는 정상적인데 그 반대일때는 거꾸로 애니메이션이 작동한다.
+        //그래서 y좌표기준이 반대일때를 지정해서 넣었더니 그래도 안 된다... 왜 그럴까..
+        
+         if (dir.z > Vector3.zero.z)
+         {
+             if(transform.rotation.y <=90 && transform.rotation.y >= -90)
+             ChangeState(State.Run);
+             else ChangeState(State.Back);
+        }
+         else if (dir.z < Vector3.zero.z)
+         {
+            if (transform.rotation.y <= 90 && transform.rotation.y >= -90)
+            {
+                ChangeState(State.Back);
+                Debug.Log("dfasd");
+            }
+            else ChangeState(State.Run); 
+        }
+         else
+         {
+             ChangeState(State.Idle);
+         }
+        
+
+        if (cc.collisionFlags == CollisionFlags.Below) //만약 player가 바닥에 닿는다면
+        {
+            jumpCount = 0; // 점프카운트를 0으로
+            yVelocity = 0; //중력을 0으로
+        }
+
+        if (Input.GetButtonDown("Jump")) // 만약 점프키(space bar)누르면
+        {
+            
+            if (jumpCount==0 && cc.collisionFlags != CollisionFlags.Below) //만약 점프키카운트가 0인 동시에 player가 바닥에 닿지 않는다면
+            {
+                return;
+            }
+            else if (jumpCount< maxJumpCount) //그렇지 않고 만약 점프카운트가 최대횟수보다 작다면
+            {
+                yVelocity = jumpPower;//중력 = 점프파워
+                jumpCount++; // 점프카운트를 1증가
+            }
+        }
+        
+        yVelocity += gravity * Time.deltaTime; // v=v0+at
+        dir.y = yVelocity;//y방향이 없었는데, y에 중력을 적용함
+        // transform.position += dir * playerMovespeed * Time.deltaTime;//p=p0*vt
+   
+        cc.Move(dir * playerMovespeed * Time.deltaTime);
+    }
+
+    void ChangeState(State state)
+    {
+        if (IsState(state))
+            return;
+        m_state = state;
+
+        m_animator.SetInteger(c_animHashKeyState, (int)m_state);
+
+
+        //{{상태가 바뀔 때 한번만 처리되어야 하는 것들은 여기서 처리!
+        switch (m_state)
+        {
+            case State.Idle:
+                break;
+            case State Run:
+                break;
+        }
+        //}}
+    }
+
+    bool IsState(State state)
+    {
+        return m_state == state;
+    }
+}
